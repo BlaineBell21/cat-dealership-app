143b50c370a6c5c47eb987d8f870950d60a7e5ef
Blaine Bell <blainebell21@gmail.com>
2026-05-14 14:10:37 -0700

    added changes to the display header

--- src/main/java/com/skills4it/dealership/ui/UserInterface.java
+++ src/main/java/com/skills4it/dealership/ui/UserInterface.java
@@ -40,7 +40,7 @@ public class UserInterface {
     private void displayHeader() {
         System.out.println();
         System.out.println("=============================================");
-        System.out.println(dealership.getName());
+        System.out.println("Welcome to this app: " + dealership.getName());
         System.out.println(dealership.getAddress() + " | " + dealership.getPhone());
         System.out.println("=============================================");
     }
